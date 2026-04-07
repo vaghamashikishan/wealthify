13b29636c1aa2dea8a88ff5efb8912c62ecce295
Kishan Vaghamashi <82988624+vaghamashikishan@users.noreply.github.com>
2026-04-07 09:26:44 +0530

    Initialize User properties with default values for improved consistency

--- Entity/User.cs
+++ Entity/User.cs
@@ -5,9 +5,9 @@ namespace wealthify.Entity;
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public bool IsFamilyHead { get; set; } = false;
     public bool IsActive { get; set; } = true;
     public DateTime CreatedAt { get; set; }
